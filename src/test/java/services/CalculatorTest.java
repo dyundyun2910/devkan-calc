7d477904fe5222a1835d0db979678dd78c4dc8e1
Jun Takaki <dyundyun2910@gmail.com>
2013-07-07 13:57:28 +0900

    Add:CalculaterTest::testAdd1And3

--- src/test/java/services/CalculatorTest.java
+++ src/test/java/services/CalculatorTest.java
@@ -8,9 +8,16 @@ import static org.junit.Assert.assertThat;
 public class CalculatorTest {
 
     @Test
-    public void testAdd() {
+    public void testAdd1And1() {
         Calculator sut = new Calculator();
         String actual = sut.add(1, 1);
         assertThat(actual, is("2"));
+    }
+
+    @Test
+    public void testAdd1And3() {
+        Calculator sut = new Calculator();
+        String actual = sut.add(1, 3);
+        assertThat(actual, is("4"));
     }
 }
